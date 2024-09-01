b42003ba0174748b8e7c6dfd9e299da771e354fb
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-09-01 14:18:14 +0530

    project is done and dusted

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import NewsBoard from './components/NewsBoard'
+function App() {
+
+
+  const [category,setCategory] = useState("entertainment")
+  return (
+    <div>
+
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem';
+
+function NewsBoard({category}) {
+
+    const [articles,setArticles] = useState([]);
+
+    useEffect(() => {
+        // let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${import.meta.env.VITE_API_KEY}`;
+        // let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+
+        let url = `https://api.mediastack.com/v1/news?access_key=66d7a494b391e84f5987753577a84902&categories=${category}&countries=us,gb,de`
+        fetch(url)
+        .then(response => response.json())
+        .then(data => setArticles(data.data))
+    },[category])
+
+    if (articles.length === 0) {
+            
+        return (
+
+        <div>
+         <h2 className='text-center m-1'>Latest <span className='badge bg-danger'>News</span></h2>
+        <div className="d-flex justify-content-center  mt-3" style={{ height: '100vh' }}>
+            <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        </div>
+        
+        )
+   
+    }
+
+  return (
+
+    <div>
+
+        <h2 className='text-center m-1'>Latest <span className='badge bg-danger'>News</span></h2>
+
+        <div>
+    
+
+                {articles.map((news, index) => {
+                    // Only render NewsItem if news.image is not null
+                    if (news.image) {
+                        return (
+                            <NewsItem 
+                                key={index} 
+                                title={news.title} 
+                                description={news.description} 
+                                src={news.image} 
+                                url={news.url} 
+                            />
+                        );
+                    }
+                    return null; // Return null if news.image is null, meaning nothing is rendered
+                })}
+            </div>
+    </div>
+  )
+}
+
+export default NewsBoard
